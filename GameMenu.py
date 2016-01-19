d814b4d1d2680ad4490f30ffa43b2e7159b8e9d6
0912293 <Kevin181094@gmail.com>
2016-01-19 15:18:59 +0100

    added exit button

--- GameMenu.py
+++ GameMenu.py
@@ -17,6 +17,7 @@ class GameMenu():
 
 
         while mainloop:
+            pygame.display.flip()
             # Limit frame speed to 50 FPS
             self.clock.tick(50)
 
@@ -27,21 +28,24 @@ class GameMenu():
             # Redraw the background
             self.screen.fill(self.bg_color)
 
-            #----button----#
             rect=screen.fill(some_color, small_rect)
+
+            my_font = pygame.font.SysFont("Arial", 70)    #creates font with font courier size 16
+            the_text = my_font.render("EXIT", True, (0,0,0))   # Text, AA , color
+            screen.blit(the_text, (220,125))     # draws text at 10,10
+
+            #----button----#
             (b1,b2,b3) = pygame.mouse.get_pressed()
             mpos = pygame.mouse.get_pos()
 
             if rect.collidepoint(mpos) & b1==1:
-                print('test test')
+                break                   #exits game
             #----button end----#
 
 
-            pygame.display.flip()
-
 if __name__ == "__main__":
     # Creating the screen
-    screen = pygame.display.set_mode((640, 480), 0, 32)
+    screen = pygame.display.set_mode((860, 640), 0, 32)
     pygame.display.set_caption('Game Menu')
     gm = GameMenu(screen)
     gm.run()
