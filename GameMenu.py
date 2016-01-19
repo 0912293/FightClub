69afc5aa648c72bab2ee4cfe40bedf069cf38e82
0912293 <Kevin181094@gmail.com>
2016-01-19 10:24:04 +0100

    added menu

---
+++ GameMenu.py
@@ -0,0 +1,31 @@
+import pygame
+
+pygame.init()
+
+class GameMenu():
+    def __init__(self, screen, bg_color=(0,0,0)):
+
+        self.screen = screen
+        self.bg_color = bg_color
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        mainloop = True
+        while mainloop:
+            # Limit frame speed to 50 FPS
+            self.clock.tick(50)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    mainloop = False
+
+            # Redraw the background
+            self.screen.fill(self.bg_color)
+            pygame.display.flip()
+
+if __name__ == "__main__":
+    # Creating the screen
+    screen = pygame.display.set_mode((640, 480), 0, 32)
+    pygame.display.set_caption('Game Menu')
+    gm = GameMenu(screen)
+    gm.run()
