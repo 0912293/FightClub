6a2e2a9ca991a848e942a9451d051a63ab98695d
0912293 <Kevin181094@gmail.com>
2016-01-20 14:56:31 +0100

    added background to menu

--- GameMenu.py
+++ GameMenu.py
@@ -23,10 +23,10 @@ def main():
     # pygame.display.toggle_fullscreen()
     # Create surface of (width, height), and its window.
     main_surface = screen
-
+    background = pygame.image.load('logo_super.png')
     exit_rect = (screenX-screenX / 2 - 250, screenY-screenY /2, 500, 75)    # (x, y, size x, size y)
     start_rect = (screenX-screenX / 2 - 250, screenY-screenY /2-80, 500, 75)
-    some_color = ( 255, 255, 255)            # A color is a mix of (Red, Green, Blue)
+    some_color = ( 0, 0, 0)            # A color is a mix of (Red, Green, Blue)
 
 
     test = 0
@@ -37,14 +37,14 @@ def main():
         if ev.type == pygame.QUIT or pygame.key.get_pressed()[27] == 1:
             break
         main_surface.fill((0, 0, 0))
-
+        main_surface.blit(background,(screenX-screenX/2 - 750, -200))
         start_button=screen.fill(some_color, start_rect)
         exit_button=screen.fill(some_color, exit_rect)
 
         #----text----#
         my_font = pygame.font.SysFont("Arial", 70)
-        startB_text = my_font.render('START', True, (0,0,0))
-        exitB_text = my_font.render("EXIT", True, (0,0,0))   # Text, AA , color
+        startB_text = my_font.render('START', True, (255,255,255))
+        exitB_text = my_font.render("EXIT", True, (255,255,255))   # Text, AA , color
         screen.blit(startB_text,(screenX-screenX / 2 - 115,screenY-screenY /2-80))
         screen.blit(exitB_text, (screenX-screenX / 2 - 80, screenY-screenY /2))     # draws text at 10,10
         #----text end---#
