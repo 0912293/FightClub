9ab37146e414b44aa50da6e6b1b05609a32f277a
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2016-01-21 00:41:50 +0100

    Improved board visuals, improved code efficiency, removed redundant code

--- Game.py
+++ Game.py
@@ -39,28 +39,31 @@ def createBoard(tiles, screenY, screenX, main_surface):
         for j in range(tiles):
             tile = (screenX//tiles*j, screenY//tiles*i, screenX//tiles, screenY//tiles)
             if i == 0 or j == 0 or i == tiles-1 or j == tiles-1:
-                color = (255, 0, 255)
+                color = (0, 200, 255)
                 width = 10
                 if (i == 0 and j == 0) or (i == tiles-1 and j == 0) or (i == 0 and j == tiles-1) or (i == tiles-1 and j == tiles-1):
-                    color = (0,255,0)
-                    width = 10
+                    pygame.draw.rect(main_surface,(0,255,0),tile)
+                else:
+                    pygame.draw.rect(main_surface,(0,200,255),tile, 10)
             else:
-                color = (255, 0, 0)
-                width = 0
-            pygame.draw.rect(main_surface,color,tile, width)
+                pygame.draw.rect(main_surface,(0, 0, 0),tile)
     center = pygame.transform.scale(pygame.image.load('BoardCenter.png'), (screenX-(screenX//tiles*2), screenY-(screenY//tiles*2)))
     main_surface.blit(center, (screenX//tiles,screenY//tiles))
 
-            # pygame.display.set_mode((screenX, screenY)).fill((255, 0, 255), tile) 
-
-# tilelist = Tile(0, True, "Regular", 0, 0)
-# tilelist = drawBoard(0, 4)
-# print(type(tilelist), tilelist.Id)
+def menu():
+    pass
 
 def main():
     """ Set up the game and run the main loop """
     pygame.init()      # Prepare the pygame module for use
     pygame.display.set_caption('Fightclub')
+
+    try:
+        pygame.mixer.music.load('beep.mp3') # muziek
+        pygame.mixer.music.play(-1, 0.0)
+    except:
+        pass
+
     if pygame.display.list_modes()[0] == (2880, 1800) or pygame.display.list_modes()[0] == (2560, 1600):
     	HDPI = 2
     	screenX, screenY = pygame.display.list_modes()[0]
@@ -72,20 +75,14 @@ def main():
     screenX -= 100
     screenY -= 100
     screen = pygame.display.set_mode((screenX, screenY))
-
     main_surface = screen
-    # Set up some data to describe a small rectangle and its color
-    small_rect = (100, 100, screenX-200, screenY-200)
-    some_color = (255, 255, 255)
-    # tilelist = [drawBoard(0)]
-    # print(tilelist.Id, tilelist.Type, tilelist.PositionX, tilelist.PositionY)
 
     while True:
         pygame.display.flip()
         ev = pygame.event.poll()
         if ev.type == pygame.QUIT or pygame.key.get_pressed()[27] == 1:
             break
-        main_surface.fill((0, 200, 255))
+        main_surface.fill((255, 255, 255))
         createBoard(11, screenY, screenX, main_surface)
 
         my_font = pygame.font.SysFont("Arial", 16)
