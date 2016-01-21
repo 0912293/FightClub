bb4c2aa4f16375e02ab3417edc6e422eb01e73f0
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2016-01-21 23:39:24 +0100

    Added support for multiple laps around the board, improved code, removed redundant code

--- Game.py
+++ Game.py
@@ -9,10 +9,10 @@ dname = os.path.dirname(abspath)
 os.chdir(dname)
 
 class Tile():
-	def __init__(self, id, x, y):
-		self.Id = id
-		self.X = x
-		self.Y = y
+    def __init__(self, id, x, y):
+        self.Id = id
+        self.X = x
+        self.Y = y
 
 def createBoard(screen):
     # tilelist = {"": ""}
@@ -34,34 +34,34 @@ def createBoard(screen):
     screen.blit(center, (screenX//tiles,screenY//tiles))
 
 def createList(s, i, j):
-	maxtile = tiles-1
+    maxtile = tiles-1
 
-	if i == 0 and j == 0:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i, j+1)
-	elif i == 0 and j > 0 and j < maxtile:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i, j+1)
-	elif i == 0 and j == maxtile:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i+1, j)
-	elif i > 0 and i < maxtile and j == maxtile:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i+1, j)
-	elif i == maxtile and j == maxtile:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i, j-1)
-	elif i == maxtile and j > 0 and j < maxtile:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i, j-1)
-	elif i == maxtile and j == 0:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i-1, j)
-	elif i == 1 and j == 0:
-		tilelist[s] = Tile(s, i, j)
-	elif i > 0 and i < maxtile and j == 0:
-		tilelist[s] = Tile(s, i, j)
-		createList(s+1, i-1, j)
+    if i == 0 and j == 0:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i, j+1)
+    elif i == 0 and j > 0 and j < maxtile:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i, j+1)
+    elif i == 0 and j == maxtile:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i+1, j)
+    elif i > 0 and i < maxtile and j == maxtile:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i+1, j)
+    elif i == maxtile and j == maxtile:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i, j-1)
+    elif i == maxtile and j > 0 and j < maxtile:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i, j-1)
+    elif i == maxtile and j == 0:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i-1, j)
+    elif i == 1 and j == 0:
+        tilelist[s] = Tile(s, i, j)
+    elif i > 0 and i < maxtile and j == 0:
+        tilelist[s] = Tile(s, i, j)
+        createList(s+1, i-1, j)
 
 def menu():
     pygame.display.flip()
@@ -97,7 +97,7 @@ def menu():
     if start_button.collidepoint(mpos) and b1==1:
         return
     if instruct_button.collidepoint(mpos) & b1==1:
-    	instructions()
+        instructions()
     if exit_button.collidepoint(mpos) and b1==1:
         pygame.quit()
     if pygame.key.get_pressed()[113] == 1:
@@ -106,81 +106,79 @@ def menu():
     menu()
 
 def instructions():
-	pygame.display.flip()
-	black = (0, 0, 0)
-	red = (150, 0, 0)
-	screen.fill(red)
-	logotexture = pygame.transform.scale(pygame.image.load('logo_super.png'), (screenX, screenY))
-	screen.blit(logotexture, (0,0))
+    pygame.display.flip()
+    black = (0, 0, 0)
+    red = (150, 0, 0)
+    screen.fill(red)
+    logotexture = pygame.transform.scale(pygame.image.load('logo_super.png'), (screenX, screenY))
+    screen.blit(logotexture, (0,0))
 
-	background_rect = (0, 50, screenX, screenY-200)
-	background=screen.fill(black, background_rect)
-	return_rect = (0, screenY-100, screenX, 75)
-	return_button=screen.fill(black, return_rect)
-	#text Text, AA , color
-	font = pygame.font.SysFont("Arial", 40)
-	background_text1 = font.render("1.Press the escape key to open the menu", True, (255,255,255))
-	background_text2 = font.render("2.To move your pawn press the 'Roll dice' button", True, (255,255,255))
-	font = pygame.font.SysFont("Arial", 70)
-	return_text = font.render('RETURN', True, (255,255,255))
-	#draw text
-	screen.blit(background_text1,(screenX//2-(len("1.Press the escape key to open the menu")*8), 150))
-	screen.blit(background_text2,(screenX//2-(len("2.To move your pawn press the 'Roll dice' button")*8), 250))
-	screen.blit(return_text, (screenX//2-(len("RETURN")*20), screenY-100))
+    background_rect = (0, 50, screenX, screenY-200)
+    background=screen.fill(black, background_rect)
+    return_rect = (0, screenY-100, screenX, 75)
+    return_button=screen.fill(black, return_rect)
+    #text Text, AA , color
+    font = pygame.font.SysFont("Arial", 40)
+    background_text1 = font.render("1.Press the escape key to open the menu", True, (255,255,255))
+    background_text2 = font.render("2.To move your pawn press the 'Roll dice' button", True, (255,255,255))
+    font = pygame.font.SysFont("Arial", 70)
+    return_text = font.render('RETURN', True, (255,255,255))
+    #draw text
+    screen.blit(background_text1,(screenX//2-(len("1.Press the escape key to open the menu")*8), 150))
+    screen.blit(background_text2,(screenX//2-(len("2.To move your pawn press the 'Roll dice' button")*8), 250))
+    screen.blit(return_text, (screenX//2-(len("RETURN")*20), screenY-100))
 
-	#button actions
-	(b1,b2,b3) = pygame.mouse.get_pressed()
-	mpos = pygame.mouse.get_pos()
-	if return_button.collidepoint(mpos) and b1==1:
-		return
-	if pygame.key.get_pressed()[27] == 1:
-		return
-	if pygame.key.get_pressed()[113] == 1:
-		pygame.quit()
-	pygame.event.wait()
-	instructions()
+    #button actions
+    (b1,b2,b3) = pygame.mouse.get_pressed()
+    mpos = pygame.mouse.get_pos()
+    if return_button.collidepoint(mpos) and b1==1:
+        return
+    if pygame.key.get_pressed()[27] == 1:
+        return
+    if pygame.key.get_pressed()[113] == 1:
+        pygame.quit()
+    pygame.event.wait()
+    instructions()
 
 class Pawn():
-	def __init__(self, id, sprite, position, x, y):
-		self.Id = id
-		self.Sprite = sprite
-		self.Position = position
-		self.X = x
-		self.Y = y
+    def __init__(self, id, sprite, position, x, y):
+        self.Id = id
+        self.Sprite = sprite
+        self.Position = position
+        self.X = x
+        self.Y = y
 
 def pawnCreate():
-	pawns = {"":""}
-	for s in range(0, players):
-		if s == 0:
-			pawnImg = "pawn24bit1.png"
-		elif s == 1:
-			pawnImg = "pawn24bit2.png"
-		elif s == 2:
-			pawnImg = "pawn24bit3.png"
-		else:
-			pawnImg = "pawn24bit4.png"
-		pawns[s] = Pawn(s, pygame.transform.scale(pygame.image.load(pawnImg), (screenX//tiles, screenY//tiles)), 1*s, 1*s, 1*s)
-		print(pawns[s].Id, pawns[s].Position)
-	global pawns
+    pawns = {"":""}
+    for s in range(0, players):
+        if s == 0:
+            pawnImg = "pawn24bit1.png"
+        elif s == 1:
+            pawnImg = "pawn24bit2.png"
+        elif s == 2:
+            pawnImg = "pawn24bit3.png"
+        else:
+            pawnImg = "pawn24bit4.png"
+        pawns[s] = Pawn(s, pygame.transform.scale(pygame.image.load(pawnImg), (screenX//tiles, screenY//tiles)), 1*s, 1*s, 1*s)
+    global pawns
 
 def pawnMove(s, forward):
-	pawns[s].Position += forward
-	print("Position", pawns[s].Position)
-	for i in range(len(tilelist)):
-		print("Tile:", tilelist[i].Id, tilelist[i].X, tilelist[i].Y)
-		if tilelist[i].Id == pawns[s].Position:
-			pawns[s].X = tilelist[i].X*(screenX//tiles)
-			pawns[s].Y = tilelist[i].Y*(screenY//tiles)
-			break
-	print("Tile:", tilelist[i].Id)
-	global pawns
+    if pawns[s].Position + forward > len(tilelist)-1:
+        remainder = pawns[s].Position + forward - len(tilelist)-1
+        pawns[s].Position = remainder
+    pawns[s].Position += forward
+    for i in range(len(tilelist)-1):
+        if tilelist[i].Id == pawns[s].Position:
+            pawns[s].X = tilelist[i].X*(screenX//tiles)
+            pawns[s].Y = tilelist[i].Y*(screenY//tiles)
+            break
 
 def main():
     pygame.init()      # Prepare the pygame module for use
     pygame.display.set_caption('Fightclub')
     
     # for i in range(0,200):
-    # 	print(pygame.key.name(i), i)
+    #   print(pygame.key.name(i), i)
 
     try:
         pygame.mixer.music.load('beep.mp3') # Attempts to play music
@@ -198,9 +196,9 @@ def main():
 
     screenX, screenY = pygame.display.list_modes()[0]
     if pygame.display.list_modes()[0] == (2880, 1800) or pygame.display.list_modes()[0] == (2560, 1600):
-    	HDPI = 2
-    	screenX = screenX//HDPI
-    	screenY = screenY//HDPI
+        HDPI = 2
+        screenX = screenX//HDPI
+        screenY = screenY//HDPI
     screenX -= 100
     screenY -= 100
     screen = pygame.display.set_mode((screenX, screenY))
@@ -227,21 +225,21 @@ def main():
         screen.blit(dice_text, (screenX//tiles, screenY//tiles))
 
         for s in range(players):
-        	pygame.event.wait()
-        	(b1,b2,b3) = pygame.mouse.get_pressed()
-        	mpos = pygame.mouse.get_pos()
-	        if dice_button.collidepoint(mpos) and b1 == 1:
-	        	diceRoll = random.randint(1,6)
-	        	pawnMove(s, 1)
-	        	# if pawns[s].Position + diceRoll >= len(tilelist):
-	        	# 	diceRoll -= (pawns[s].Position + diceRoll - len(tilelist))
-	        	# 	pawnMove(s, diceRoll)
-	        	# 	pawns[s].Position = 0
-	        	# else:
-	        	# 	pawnMove(s, diceRoll)
+            pygame.event.wait()
+            (b1,b2,b3) = pygame.mouse.get_pressed()
+            mpos = pygame.mouse.get_pos()
+            if dice_button.collidepoint(mpos) and b1 == 1:
+                diceRoll = random.randint(1,6)
+                pawnMove(s, 1)
+                # if pawns[s].Position + diceRoll >= len(tilelist):
+                #   diceRoll -= (pawns[s].Position + diceRoll - len(tilelist))
+                #   pawnMove(s, diceRoll)
+                #   pawns[s].Position = 0
+                # else:
+                #   pawnMove(s, diceRoll)
 
         for s in range(players):
-        	screen.blit(pawns[s].Sprite, (pawns[s].X, pawns[s].Y))
+            screen.blit(pawns[s].Sprite, (pawns[s].X, pawns[s].Y))
 
         my_font = pygame.font.SysFont("Arial", 16)
         the_text = my_font.render("Dice: {0}".format(diceRoll), True, (0,0,0))   # Text, AA , color
