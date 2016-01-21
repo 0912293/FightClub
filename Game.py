b49d1ed34c34bbd56942dcb484ac50016fd3976a
Floris-Jan Willemsen <fjwillemsen@icloud.com>
2016-01-21 01:42:25 +0100

    Added a menu to the game, removed useless code, cleaned up code

--- Game.py
+++ Game.py
@@ -7,33 +7,7 @@ abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 os.chdir(dname)
 
-class Tile:
-    def __init__(self, id, type, first, positionX, positionY):
-        self.Id = id
-        self.Type = type
-        self.First = first
-        self.PositionX = positionX
-        self.PositionY = positionY
-
-def drawBoard(tid, max):
-    if tid == 0:
-        ttype = "Regular"
-        first = True
-        positionX = 0
-        positionY = 0
-    else:
-        ttype = "Regular"
-        first = False
-        positionX = positionX + 300
-        positionY
-    global positionX
-    global positionY
-    if tid < max:
-        return Tile(tid, ttype, first, positionX, positionY), drawBoard(tid+1, max)
-    else:
-        return Tile(tid, ttype, first, positionX, positionY)
-
-def createBoard(tiles, screenY, screenX, main_surface):
+def createBoard(main_surface):
     tilelist = []
     for i in range(tiles):
         for j in range(tiles):
@@ -50,11 +24,37 @@ def createBoard(tiles, screenY, screenX, main_surface):
     center = pygame.transform.scale(pygame.image.load('BoardCenter.png'), (screenX-(screenX//tiles*2), screenY-(screenY//tiles*2)))
     main_surface.blit(center, (screenX//tiles,screenY//tiles))
 
-def menu():
-    pass
+def menu(main_surface):
+    pygame.display.flip()
+    black = (0, 0, 0)
+    main_surface.fill(black)
+    logotexture = pygame.transform.scale(pygame.image.load('logo_super.png'), (screenX, screenY))
+    main_surface.blit(logotexture, (0,0))
+
+    #buttons
+    exit_rect = (screenX-screenX / 2 - 250, screenY-screenY /2, 500, 75)    # (x, y, size x, size y)
+    start_rect = (screenX-screenX / 2 - 250, screenY-screenY /2-80, 500, 75)
+    start_button=main_surface.fill(black, start_rect)
+    exit_button=main_surface.fill(black, exit_rect)
+
+    #text
+    my_font = pygame.font.SysFont("Arial", 70)
+    startB_text = my_font.render('START', True, (255,255,255))
+    exitB_text = my_font.render("EXIT", True, (255,255,255))   # Text, AA , color
+    main_surface.blit(startB_text,(screenX-screenX / 2 - 115,screenY-screenY /2-80))
+    main_surface.blit(exitB_text, (screenX-screenX / 2 - 80, screenY-screenY /2))     # draws text at 10,10
+
+    #button actions
+    (b1,b2,b3) = pygame.mouse.get_pressed()
+    mpos = pygame.mouse.get_pos()
+    if start_button.collidepoint(mpos) and b1==1:
+        return
+    if exit_button.collidepoint(mpos) & b1==1:
+        pygame.quit()
+    pygame.event.wait()
+    menu(main_surface)
 
 def main():
-    """ Set up the game and run the main loop """
     pygame.init()      # Prepare the pygame module for use
     pygame.display.set_caption('Fightclub')
 
@@ -64,6 +64,8 @@ def main():
     except:
         pass
 
+    tiles = 11
+    global tiles
     if pygame.display.list_modes()[0] == (2880, 1800) or pygame.display.list_modes()[0] == (2560, 1600):
     	HDPI = 2
     	screenX, screenY = pygame.display.list_modes()[0]
@@ -74,26 +76,28 @@ def main():
     	screenX, screenY = pygame.display.list_modes()[0]
     screenX -= 100
     screenY -= 100
+    global screenX
+    global screenY
     screen = pygame.display.set_mode((screenX, screenY))
     main_surface = screen
 
     while True:
         pygame.display.flip()
         ev = pygame.event.poll()
-        if ev.type == pygame.QUIT or pygame.key.get_pressed()[27] == 1:
+        if ev.type == pygame.QUIT:
             break
         main_surface.fill((255, 255, 255))
-        createBoard(11, screenY, screenX, main_surface)
+        createBoard(main_surface)
 
         my_font = pygame.font.SysFont("Arial", 16)
         the_text = my_font.render("test: {0}".format("Hello World"), True, (0,0,0))   # Text, AA , color
         screen.blit(the_text, (10, 10))     # draws text at 10,10
-        # main_surface.fill(some_color, small_rect)
-
+        if pygame.key.get_pressed()[27] == 1:
+            menu(main_surface)
         my_clock = pygame.time.Clock()
         my_clock.tick(60)
 
-
+    pygame.mixer.music.stop()
     pygame.quit()     # Once we leave the loop, close the window.
 
 main()
