fb97872e861f11cfadee5fac09f4d17c5a54730b
vincent <vincentpruyn@telfort.nl>
2016-01-22 10:32:25 +0100

    supercard names

--- Cards.py
+++ Cards.py
@@ -14,4 +14,4 @@ class Cards():
         self.Name = cardDict[id]
         self.Sprite = os.path.join("img", "sf"+id+".png")
 
-cardDict = {0:"First", 1:"Second"}
+cardDict = {0:"Terry Crews", 1:"Jason Statham", 2:"Wesley Sniper", 3:"Jet Ri", 4:"Steve Seagal", 5:"Super Merio", 6:"Vin Dieser", 7:"Chack Norris", 8:"The Roch", 9:"Ernold Schwarzenegger", 10:"Pariz Hilten", 11:"Jackie Chen", 12:"Steve Urkel", 13:"John Cena", 14:"James Bend", 15:"Dexter", 16:"Agua Man", 17:"Bruce Hee"}
