6b39e3523b1b856532e5c27a294101980c588e43
vincent <vincentpruyn@telfort.nl>
2016-01-22 11:15:17 +0100

    supercard damages updated

--- Cards.py
+++ Cards.py
@@ -15,5 +15,9 @@ class Cards():
         self.Damage = cardDamage[id]
         self.Sprite = os.path.join("img", "sf"+id+".png")
 
-cardNameDict = {0:"Terry Crews", 1:"Jason Statham", 2:"Wesley Sniper", 3:"Jet Ri", 4:"Steve Seagal", 5:"Super Merio", 6:"Vin Dieser", 7:"Chack Norris", 8:"The Roch", 9:"Ernold Schwarzenegger", 10:"Pariz Hilten", 11:"Jackie Chen", 12:"Steve Urkel", 13:"John Cena", 14:"James Bend", 15:"Dexter", 16:"Agua Man", 17:"Bruce Hee"}
-cardDamage = {0:16, 1:20, 2:13, 3:19, 4:16, 5:18, 6:23, 7:26, 8:18, 9:18, 10:11, 11:14, 12:10, 13:11, 14:18, 15:10, 16:11, 17:14}
+cardDict = {0:"Terry Crews", 1:"Jason Statham", 2:"Wesley Sniper", 3:"Jet Ri", 4:"Steve Seagal",
+                5:"Super Merio", 6:"Vin Dieser", 7:"Chack Norris", 8:"The Roch", 9:"Ernold Schwarzenegger",
+                10:"Pariz Hilten", 11:"Jackie Chen", 12:"Steve Urkel", 13:"John Cena", 14:"James Bend",
+                15:"Dexter", 16:"Agua Man", 17:"Bruce Hee"}
+cardDamage = {0:16, 1:18, 2:13, 3:20, 4:16, 5:15, 6:13, 7:32, 8:24, 9:26,
+              10:6, 11:9, 12:7, 13:26, 14:28, 15:13, 16:7, 17:29}
