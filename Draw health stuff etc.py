441ae39d3c476a0ce00a10824fd4c41f98037706
0912293 <Kevin181094@gmail.com>
2016-01-25 12:32:00 +0100

    added health,stamina,player draw

--- Draw health stuff etc.py
+++ Draw health stuff etc.py
@@ -29,8 +29,8 @@ def createBoard():
                 # s += 1
                 # global tilelist
             else:
-                pygame.draw.rect(screen,(0, 0, 0),tile)
-    center = pygame.transform.scale(pygame.image.load('BoardCenter.png'), (screenX-(screenX//tiles*2), screenY-(screenY//tiles*2)))
+                pygame.draw.rect(screen,(255, 255, 255),tile)
+    center = pygame.transform.scale(pygame.image.load('logo_super.png'), (screenX-(screenX//tiles*2), screenY-(screenY//tiles*2)))
     screen.blit(center, (screenX//tiles,screenY//tiles))
 
 def createList(s, i, j):
@@ -74,7 +74,7 @@ def menu():
     #buttons (x, y, size x, size y)
     start_rect = (0, 50, screenX, 75)
     instruct_rect = (0, 150, screenX, 75)
-    exit_rect = (0, 250, screenX, 75)
+    exit_rect = (0, 250, screenX, 75) 
 
     start_button=screen.fill(black, start_rect)
     instruct_button=screen.fill(black, instruct_rect)
@@ -85,7 +85,7 @@ def menu():
     startB_text = my_font.render('START', True, (255,255,255))
     instructB_text = my_font.render('INSTRUCTIONS', True, (255,255,255))
     exitB_text = my_font.render("EXIT", True, (255,255,255))
-
+    
     #draw text
     screen.blit(startB_text,(screenX//2-(len("START")*20), 50))
     screen.blit(instructB_text,(screenX//2-(len("INSTRUCTIONS")*20), 150))
@@ -141,7 +141,7 @@ def instructions():
     instructions()
 
 class Player():
-    def __init__(self, id, sprite, position, x, y, health, stamina, name):
+    def __init__(self, id, sprite, position, x, y, health, stamina, name, color):
         self.Id = id
         self.Sprite = sprite
         self.Position = position
@@ -150,11 +150,12 @@ class Player():
         self.Health = health
         self.Stamina = stamina
         self.Name = name
+        self.Color = color[id]
 
 def playerCreate():
     players = {"":""}
     for s in range(0, numberOfPlayers):
-        players[s] = Player(s, pygame.transform.scale(pygame.image.load(os.path.join("pawn24bit" + str(s+1) + ".png")), (screenX//tiles, screenY//tiles)), 1*s, 1*s, 1*s, 100, 15, "name")
+        players[s] = Player(s, pygame.transform.scale(pygame.image.load(os.path.join("pawn24bit" + str(s+1) + ".png")), (screenX//tiles, screenY//tiles)), 1*s, 1*s, 1*s, 100, 15, "name", playerColors)
     global players
 
 def playerMove(s, forward):
@@ -167,50 +168,35 @@ def playerMove(s, forward):
             players[s].X = tilelist[i].X*(screenX//tiles)
             players[s].Y = tilelist[i].Y*(screenY//tiles)
             pygame.display.flip()
-            print('player', s, players[s].Health)
-            print("Player", s)
-            finish()
             break
 
-def turn(player, font):
-    dice_rect = (screenX//tiles, screenY//tiles, 500, 75)
-    dice_button=screen.fill((150,0,0), dice_rect)
-    dice_text = font.render("Roll dice", True, (255,255,255))
-    screen.blit(dice_text, (screenX//tiles, screenY//tiles))
-    (b1,b2,b3) = pygame.mouse.get_pressed()
-    mpos = pygame.mouse.get_pos()
-    forward = 1
-    if dice_button.collidepoint(mpos) and b1 == 1:
-        if player == numberOfPlayers-1:
-            return playerMove(player, forward)
-        elif player < numberOfPlayers-1:
-            playerMove(player, forward)
-            turn(player+1, font)
-    # else:
-    #     turn(player+1, font)
-
-def finish():
-    black = (0, 0, 0)
+def player_stats(s):
     pygame.display.flip()
     font = pygame.font.SysFont('Arial',60,True)
-    text = font.render("Game finished", True, (255,255,255))
-    exitB_text = font.render("EXIT", True, (255,255,255))
-    exit_rect = (0, screenY//2, screenX, 75)
-    exit_button=screen.fill(black, exit_rect)
-    screen.blit(text,(screenX//2-(len('Game finished')*13), 150))
-    screen.blit(exitB_text, (screenX//2-(len("EXIT")*4), screenY//2))
-    (b1,b2,b3) = pygame.mouse.get_pressed()
-    mpos = pygame.mouse.get_pos()
-    if exit_button.collidepoint(mpos) and b1==1:
-        pygame.quit()
+    Player = font.render('Player: {0}' .format(s), True, (0,255,255))
+    Health = font.render('Health: {0}' .format(players[s].Health), True, (0,255,255))
+    Stamina = font.render('Health: {0}' .format(players[s].Stamina), True, (0,255,255))
+    screen.blit(Player, (screenX//tiles, screenY//tiles+80))
+    screen.blit(Health, (screenX//tiles, screenY//tiles+160))
+    screen.blit(Stamina, (screenX//tiles, screenY//tiles+240))
     pygame.event.wait()
-    finish()
 
+
+def turn(player):
+    forward = random.randint(1,6)
+    global playerN
+    global forward
+    if player == numberOfPlayers-1:
+        playerMove(player, forward)
+        playerN = 0
+    elif player < numberOfPlayers-1:
+        playerMove(player, forward)
+        playerN += 1
 
 def main():
     pygame.init()      # Prepare the pygame module for use
     pygame.display.set_caption('Fightclub')
-
+    
     # for i in range(0,200):
     #   print(pygame.key.name(i), i)
 
@@ -222,10 +208,16 @@ def main():
 
     tilelist = {"": ""}
     tiles = 11
-    numberOfPlayers = 3
+    numberOfPlayers = 4
+    playerN = 0
+    forward = 0
+    playerColors = {0: (189,33,50), 1: (26,118,186), 2: (15,103,59), 3: (254,220,56)}
     global tilelist
     global tiles
     global numberOfPlayers
+    global playerN
+    global forward
+    global playerColors
     createList(0,0,0)
 
     screenX, screenY = pygame.display.list_modes()[0]
@@ -239,7 +231,6 @@ def main():
     global screenX
     global screenY
     global screen
-    diceRoll = 0
     playerCreate()
     menu()
 
@@ -251,52 +242,34 @@ def main():
         screen.fill((255, 255, 255))
         createBoard()
 
-        #dice button
-
         pygame.event.pump()
-        my_font = pygame.font.SysFont("Helvetica", 70)
-        turn(0, my_font)
-        # s = 0
-        # while s < numberOfPlayers:
-        #     pygame.event.wait()
-        #     (b1,b2,b3) = pygame.mouse.get_pressed()
-        #     mpos = pygame.mouse.get_pos()
-        #     if dice_button.collidepoint(mpos) and b1 == 1:
-        #         diceRoll = random.randint(1,6)
-        #         print("Moved player", s)
-        #         playerMove(s, 1)
-        #         s += 1
-        #         pygame.display.flip()
+        font = pygame.font.SysFont("Helvetica", 70)
+        dice_rect = (screenX//tiles, screenY//tiles, 500, 75)
+        dice_button=screen.fill(players[playerN].Color, dice_rect)
+        dice_text = font.render("Roll dice", True, (255,255,255))
+        screen.blit(dice_text, (screenX//tiles, screenY//tiles))
 
-        # for s in range(numberOfPlayers):
-        #     print("Hello")
-        #     pygame.event.wait()
-        #     print("Goodbye")
-        #     (b1,b2,b3) = pygame.mouse.get_pressed()
-        #     mpos = pygame.mouse.get_pos()
-        #     if dice_button.collidepoint(mpos) and b1 == 1:
-        #         diceRoll = random.randint(1,6)
-        #         print("Moved player", s)
-        #         playerMove(s, 1)
-                # if players[s].Position + diceRoll >= len(tilelist):
-                #   diceRoll -= (players[s].Position + diceRoll - len(tilelist))
-                #   playerMove(s, diceRoll)
-                #   players[s].Position = 0
-                # else:
-                #   playerMove(s, diceRoll)
+        pygame.event.get()
+        (b1,b2,b3) = pygame.mouse.get_pressed()
+        mpos = pygame.mouse.get_pos()
+        if dice_button.collidepoint(mpos) and b1 == 1:
+            turn(playerN)
 
         for s in range(numberOfPlayers):
             screen.blit(players[s].Sprite, (players[s].X, players[s].Y))
+        player_stats(playerN)
 
         my_font = pygame.font.SysFont("Arial", 16)
-        the_text = my_font.render("Dice: {0}".format(diceRoll), True, (0,0,0))   # Text, AA , color
+        the_text = my_font.render("Dice: {0}".format(forward), True, (0,0,0))   # Text, AA , color
+        print(s)
         screen.blit(the_text, (10, 10))     # draws text at 10,10
 
+        pygame.event.get()
         if pygame.key.get_pressed()[27] == 1:
             menu()
         if pygame.key.get_pressed()[113] == 1:
             pygame.quit()
-            break
+            break 
         my_clock = pygame.time.Clock()
         my_clock.tick(60)
 
